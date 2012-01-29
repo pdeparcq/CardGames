791b638bc3cb5015e2e150e27a8333124156236d
Pieter Deparcq <pdeparcq@gmail.com>
2012-01-29 09:00:09 +0100

    Hello World

---
+++ CardGames/src/cardgames.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="GAG569"
+__date__ ="$29-jan-2012 8:54:54$"
+
+if __name__ == "__main__":
+    print "Hello World"
